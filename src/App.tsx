29b7480035ec77e1f12c8e2b1dba3f52e6612022
Siddy096 <varhadisiddhesh8@gmail.com>
2025-08-30 00:18:23 +0530

    ScrollBars Appeared

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import ContentArea from "./components/ContentArea";
 import Achievements from "./components/Achievements";
 import './App.css'
+import { Element } from 'react-scroll';
 
 function App(){
   return(
@@ -16,10 +17,12 @@ function App(){
             <div className="navbar">
               <NavBar></NavBar>
             </div>
+            <Element name = "main-content">
             <div className="content-area"> 
-              <ContentArea></ContentArea>
-              <Achievements></Achievements>
+                <ContentArea></ContentArea><br />
+                <Achievements></Achievements>
             </div>
+            </Element>
           </div>
         </div>
         <div className="address-bar">
--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -8,21 +8,21 @@ function ContentArea() {
     return (
         <div className="content-area">
                 <div className="rotating-image-container">
-                <motion.img 
-                    className="image" 
-                    src={outer} 
-                    width="300px" 
-                    height="300px" 
-                    animate={{
-                        rotate: 360
-                    }}
-                    transition={{
-                        duration: 5, 
-                        repeatType: "loop",
-                        repeat: Infinity,
-                        ease: "linear"
-                    }}
-                />
+                    <motion.img 
+                        className="image" 
+                        src={outer} 
+                        width="300px" 
+                        height="300px" 
+                        animate={{
+                            rotate: 360
+                        }}
+                        transition={{
+                            duration: 5, 
+                            repeatType: "loop",
+                            repeat: Infinity,
+                            ease: "linear"
+                        }}
+                    />
             </div>
         </div>
     )
